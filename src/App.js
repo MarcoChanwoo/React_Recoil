f81c9f3022bf1a72bb8d5d1236593c22372a245e
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-03 21:24:43 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import "./App.css";
 import TextInput from "./components/TextInput";
 import CharacterCount from "./components/CharacterCount";
@@ -6,6 +6,12 @@ import CharacterCount from "./components/CharacterCount";
 export const textState = atom({
   key: "textState",
   default: "", // 빈 값
+});
+export const charCountState = selector({
+  key: "charCountState",
+  get: ({ get }) => {
+    const test = get(textState);
+  },
 });
 
 function App() {
